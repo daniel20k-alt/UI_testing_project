c944d83ab73d568ffe0be7ed278702fd8166a781
Testing_author <testing_beta@mail.com>
2021-01-13 23:52:05 +0200

    Refactoring the code and writing an UI test.

--- DogYearsUITests/DogYearsUITests.swift
+++ DogYearsUITests/DogYearsUITests.swift
@@ -29,8 +29,11 @@
 import XCTest
 
 class DogYearsUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
+        app = XCUIApplication()
+        app.launch()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -39,27 +42,15 @@ class DogYearsUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-              
-        XCUIApplication().navigationBars["Master"].buttons["Menu"].tap()
+    func test_navigationBackToMenu() throws {
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+let masterNavBar = app.navigationBars["Master"]
+let menuButton = masterNavBar.buttons["Menu"]
+    menuButton.tap()
+         
+        XCTAssertFalse(masterNavBar.exists)
+        XCTAssert(app.navigationBars["Menu"].exists)
+                
     }
 }
